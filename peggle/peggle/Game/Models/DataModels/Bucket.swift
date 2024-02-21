6109060a85eed5182028c9fb000382d51cef5b9b
JCSnap <e0970167@u.nus.edu>
2024-02-21 16:48:44 +0800

    add bucket model

---
+++ peggle/peggle/Game/Models/DataModels/Bucket.swift
@@ -0,0 +1,14 @@
+//
+//  Bucket.swift
+//  peggle
+//
+//  Created by Justin Cheah Yun Fei on 21/2/24.
+//
+
+import Foundation
+
+struct Bucket {
+    var center: CGPoint
+    var width: CGFloat
+    var height: CGFloat
+}
