f07c85c8e1f0e6fca0029a2a97082122ba024ce3
JCSnap <e0970167@u.nus.edu>
2024-02-21 16:49:26 +0800

    add physics bucket

---
+++ peggle/peggle/Game/Models/DataModels/PhysicsBucket.swift
@@ -0,0 +1,32 @@
+//
+//  PhysicsBucket.swift
+//  peggle
+//
+//  Created by Justin Cheah Yun Fei on 21/2/24.
+//
+
+import Foundation
+
+struct PhysicsBucket: RectangularPhysicsObject {
+    var bucket: Bucket
+    var velocity: CGVector
+    var mass: CGFloat
+    var center: CGPoint {
+        get { bucket.center }
+        set { bucket.center = newValue }
+    }
+    var width: CGFloat {
+        get { bucket.width }
+        set { bucket.width = newValue }
+    }
+    var height: CGFloat {
+        get { bucket.height }
+        set { bucket.height = newValue }
+    }
+    
+    init(bucket: Bucket, velocity: CGVector = .zero, mass: CGFloat = Constants.defaultBucketMass) {
+        self.bucket = bucket
+        self.velocity = velocity
+        self.mass = mass
+    }
+}
