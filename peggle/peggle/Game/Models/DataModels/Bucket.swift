1ce005d0053f4e993e7b1373888b253c687bbdbf
JCSnap <e0970167@u.nus.edu>
2024-02-21 18:44:43 +0800

    add instantiator for bucket with default values

--- peggle/peggle/Game/Models/DataModels/Bucket.swift
+++ peggle/peggle/Game/Models/DataModels/Bucket.swift
@@ -11,4 +11,10 @@ struct Bucket {
     var center: CGPoint
     var width: CGFloat
     var height: CGFloat
+    
+    init(center: CGPoint, width: CGFloat = Constants.defaultBucketSize.width, height: CGFloat = Constants.defaultBucketSize.height) {
+        self.center = center
+        self.width = width
+        self.height = height
+    }
 }
