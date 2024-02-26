7223c5d04becdb41a04804223e485147934449d7
JCSnap <e0970167@u.nus.edu>
2024-02-26 18:03:01 +0800

    add assets for power ups

---
+++ peggle/peggle/Game/Views/Game/GameSelectPowerView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectPowerView.swift
+//  peggle
+//
+//  Created by Justin Cheah Yun Fei on 26/2/24.
+//
+
+import SwiftUI
+
+struct SelectPowerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectPowerView()
+}
---
+++ peggle/peggle/Game/Views/ReusableComponents/PowerView.swift
@@ -0,0 +1,18 @@
+//
+//  PowerView.swift
+//  peggle
+//
+//  Created by Justin Cheah Yun Fei on 26/2/24.
+//
+
+import SwiftUI
+
+struct PowerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PowerView()
+}
