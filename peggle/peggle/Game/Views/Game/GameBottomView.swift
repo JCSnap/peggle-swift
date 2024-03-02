e2c0e4512168afc2759e4a123502c7291a707b36
JCSnap <e0970167@u.nus.edu>
2024-03-02 23:34:17 +0800

    add ui to indicate power activated

--- peggle/peggle/Game/Views/Game/GameBottomView.swift
+++ peggle/peggle/Game/Views/Game/GameBottomView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct GameBottomView: View {
     @State var viewModel: GameBottomViewDelegate
+    @State var activatedPower: Bool = false
 
     var body: some View {
         HStack {
@@ -21,12 +22,39 @@ struct GameBottomView: View {
                 .font(.largeTitle)
             }
             if viewModel.canActivatePower && !viewModel.isAiming {
-                Button(action: viewModel.activatePower) {
+                Button(action: {
+                    viewModel.activatePower()
+                    activatedPower = true
+                }) {
                     PowerOnView()
                 }
             }
         }
         .frame(width: .infinity, height: 100)
+        .overlay(
+            activatedPower ? ActivatedPowerView() : nil,
+            alignment: .top
+        )
+    }
+}
+
+struct ActivatedPowerView: View {
+    @State private var opacity: CGFloat = 1.0
+    
+    var body: some View {
+        Text("Power Activated!")
+            .font(.custom("Marker Felt", size: 30))
+            .frame(width: 500, height: 200)
+            .foregroundStyle(.brown)
+            .offset(y: -150)
+            .opacity(opacity)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    withAnimation {
+                        opacity = 0
+                    }
+                }
+            }
     }
 }
 
